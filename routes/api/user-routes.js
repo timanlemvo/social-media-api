1204b1cbc4f9cede8cbb146e194872d70088ffd5
timanlemvo <timanlemvo@gmail.com>
2021-08-22 18:36:30 -0700

    bonus points round 2

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const {
     getAllUsers,
     getUserById,
-    getUserByUsername,
     createUser,
     updateUser,
     deleteUser,
@@ -19,9 +18,6 @@ router.route('/:id')
     .get(getUserById)
     .put(updateUser)
     .delete(deleteUser)
-
-router.route('/:username')
-    .get(getUserByUsername)
 
 router.route('/:userId/friends/:friendId')
     .post(addFriend)
